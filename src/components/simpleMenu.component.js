1c682b399e687974bfe4b90bd224031980b06e50
Jay Thakkar <53138874+SirAlphaMaverick@users.noreply.github.com>
2020-12-25 14:25:02 +1300

    sorting-headers

---
+++ src/components/simpleMenu.component.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+export default function SimpleMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+        <MenuIcon aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick} />
+      
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Services</MenuItem>
+        <MenuItem onClick={handleClose}>About</MenuItem>
+        <MenuItem onClick={handleClose}>Contact</MenuItem>
+        <MenuItem onClick={handleClose}>Login</MenuItem>
+      </Menu>
+    </div>
+  );
+}
