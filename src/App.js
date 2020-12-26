133d044d038c18151804683e25d5d5b95acaa7ab
Jay Thakkar <53138874+SirAlphaMaverick@users.noreply.github.com>
2020-12-26 17:06:45 +1300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header.component';
+import { Route, BrowserRouter as Router} from 'react-router-dom';
+import Services from './components/services.component';
+import About from './components/about.component';
+import Contact from './components/contact.component';
 
 function App() {
   return (
     <div className="App" >
-
-      <Header />
+<Router style={{ textDecoration: 'none' }}>
+  <Header />
+      <Route path="/services" exact component={Services} />
+      <Route path="/about" exact component={About} />
+      <Route path="/contact" exact component={Contact} />
       
-      <header className="App-header" >
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World!</h1>
-      </header>
+      </Router>
+      
 
 
     </div>
