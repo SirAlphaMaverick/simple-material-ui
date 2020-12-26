4fdf52bc77903247808cbe4eaa74b03096522d12
Jay Thakkar <53138874+SirAlphaMaverick@users.noreply.github.com>
2020-12-26 17:04:05 +1300

    routing-pages

---
+++ services.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from './header.component';
+
+class Services extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello I am bored of this.</h1>
+            </div>
+        )
+    }
+}
+
+export default Services;
